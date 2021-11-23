2d08e4796dcb872220efa7545f3357386f48ecc6
Marlow Collins <marlowcollins95@gmail.com>
2021-11-23 00:49:43 -0600

    MiddleWares Added

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const createError = require('http-errors')
 const usersRouter = require('./routes/usersRouter');
 
+const app = express();
+
+//middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+//main route
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.send('Hello this is CalenTask')
 })
 
-//middleware
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
-
 //routes
 app.use('/users', usersRouter)
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use((err, req, res, next) => {
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+// render the error page
+res.status(err.status || 500);
+res.render('error');
+});
+  
 //port
 app.listen(3001, () => console.log('Server is ready'))
