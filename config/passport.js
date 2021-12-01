05553be25d8970d58e91df56dfe1653439d7a23e
Marlow Collins <marlowcollins95@gmail.com>
2021-11-30 22:12:27 -0600

    crypto update

--- config/passport.js
+++ config/passport.js
@@ -24,21 +24,10 @@ passport.use(new LocalStrategy(
     function (username, password, done) {
         Users.findOne({ where: { username: username, password: password } })
             .then(users => {
-                if(!users) {
+                if(!users || !Users.prototype.correctPassword(req.body.password)) {
                     return done(null, false, { message: 'Incorrect Username' });
-                };
-
-                const isValid = Users.prototype.correctPassword(req.body.password)
-                
-                if (isValid) {
-                    return done(null, user);
-                } else {
-                    return done(null, false);
                 }
-                /*if(!users.password === password){
-                    return done(null, false, { message: 'Incorrect Password' });
-                }
-                return done(null, users)*/
+                return done(null, users)
             }).catch(err => done(err));
         }
     )
